c4fe92e54a22fe7121294c1ccc9c807b526eeb85
Yonatan <yonatan5556201@gmail.com>
2021-12-08 20:41:05 +0200

    changed the row

--- src/Bibi.java
+++ src/Bibi.java
@@ -1,6 +1,6 @@
 public class Bibi {
     public static void main(String[] args) {
         System.out.println("jello");
-        System.out.println("this is in red");
+        System.out.println("this is  red");
     }
 }
